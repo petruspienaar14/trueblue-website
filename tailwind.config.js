b4f46221b8ed57d1dc0b5a70438492cebbdeb03f
petruspienaar14 <petruspienaar14@gmail.com>
2025-10-19 18:07:23 +0200

    Header and Footer Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.{html,js}"],
+  content: ["./*.html", "./components/**/*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
